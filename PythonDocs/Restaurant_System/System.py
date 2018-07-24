cdaf88f1b8b7928e8cd2eb61128e52e1c1175c0b
LUKE GLOVER <41595199+lglover241@users.noreply.github.com>
2018-07-24 10:42:58 -0700

    Created Food Options

--- PythonDocs/Restaurant_System/System.py
+++ PythonDocs/Restaurant_System/System.py
@@ -9,14 +9,23 @@ root.title("Restaurant Management System")
 ##Variables##
 text_Input = StringVar()
 operator = ""
+rand = StringVar()
+Fries = StringVar()
+Burger = StringVar()
+Chx = StringVar()
+PulledPork = StringVar()
+Ribs = StringVar()
+ColeSlaw = StringVar()
+CheeseBurger = StringVar()
 
-Top = Frame(root, width = 1600,height=50,bg="darkslategray",relief=SUNKEN)
+
+Top = Frame(root, width = 1600,height=50,bg="darkseagreen",relief=SUNKEN)
 Top.pack(side=TOP)
 
-f1 = Frame(root, width = 800,height=700,bg="salmon",relief=SUNKEN)
+f1 = Frame(root, width = 800,height=700,relief=SUNKEN)
 f1.pack(side=LEFT)
 
-f2 = Frame(root, width = 500,height=800,bg="darkseagreen",relief=SUNKEN)
+f2 = Frame(root, width = 500,height=800,relief=SUNKEN)
 f2.pack(side=RIGHT)
 
 ##TIME##
@@ -86,6 +95,51 @@ btnClear=Button(f2,padx=16,pady=16,bd=8,fg="black",font=('arial',20,'bold'),text
 btnEquals=Button(f2,padx=16,pady=16,bd=8,fg="black",font=('arial',20,'bold'),text="=",bg="gray",command=btnEquals).grid(row=5,column=2)
 
 Division=Button(f2,padx=16,pady=16,bd=8,fg="black",font=('arial',20,'bold'),text="/",bg="gray",command=lambda: btnClick('/')).grid(row=5,column=3)
+
+##Food Options##
+##column 1##
+lblReference = Label(f1,font=('arial',16,'bold'),text="Reference",bd=16,anchor='e',justify='left')
+lblReference.grid(row=0,column=0)
+txtReference=Entry(f1,font=('arial',16,'bold'),textvariable=rand,bd=10,insertwidth=4,bg="darkseagreen",justify='right')
+txtReference.grid(row=0,column=1)
+
+lblFries = Label(f1,font=('arial',16,'bold'),text="Fries",bd=16,anchor='e')
+lblFries.grid(row=1,column=0)
+txtFries=Entry(f1,font=('arial',16,'bold'),textvariable=Fries,bd=10,insertwidth=4,bg="darkseagreen",justify='right')
+txtFries.grid(row=1,column=1)
+
+lblBurger = Label(f1,font=('arial',16,'bold'),text="Burger",bd=16,anchor='e')
+lblBurger.grid(row=2,column=0)
+txtBurger=Entry(f1,font=('arial',16,'bold'),textvariable=Burger,bd=10,insertwidth=4,bg="darkseagreen",justify='right')
+txtBurger.grid(row=2,column=1)
+
+lblCheeseBurger = Label(f1,font=('arial',16,'bold'),text="Cheese Burger",bd=16,anchor='e')
+lblCheeseBurger.grid(row=3,column=0)
+txtCheeseBurger=Entry(f1,font=('arial',16,'bold'),textvariable=CheeseBurger,bd=10,insertwidth=4,bg="darkseagreen",justify='right')
+txtCheeseBurger.grid(row=3,column=1)
+
+##column 2##
+lblChx = Label(f1,font=('arial',16,'bold'),text="Chicken Sandwich",bd=16,anchor='e',justify='left')
+lblChx.grid(row=4,column=0)
+txtChx=Entry(f1,font=('arial',16,'bold'),textvariable=Chx,bd=10,insertwidth=4,bg="darkseagreen",justify='right')
+txtReference.grid(row=4,column=1)
+
+lblPork = Label(f1,font=('arial',16,'bold'),text="Pulled Pork",bd=16,anchor='e')
+lblPork.grid(row=5,column=0)
+txtPork=Entry(f1,font=('arial',16,'bold'),textvariable=PulledPork,bd=10,insertwidth=4,bg="darkseagreen",justify='right')
+txtPork.grid(row=5,column=1)
+
+lblRibs = Label(f1,font=('arial',16,'bold'),text="Ribs",bd=16,anchor='e')
+lblRibs.grid(row=6,column=0)
+txtRibs=Entry(f1,font=('arial',16,'bold'),textvariable=Ribs,bd=10,insertwidth=4,bg="darkseagreen",justify='right')
+txtRibs.grid(row=6,column=1)
+
+lblSlaw = Label(f1,font=('arial',16,'bold'),text="Cole Slaw",bd=16,anchor='e')
+lblSlaw.grid(row=7,column=0)
+txtSlaw=Entry(f1,font=('arial',16,'bold'),textvariable=ColeSlaw,bd=10,insertwidth=4,bg="darkseagreen",justify='right')
+txtSlaw.grid(row=7,column=1)
+
+
 
 
 
